75db60fb444d067fbb762e7141b1f9061616e155
zhaoyang <zla143@sfu.ca>
2018-09-01 23:29:02 -0700

    password jwt

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var dishRouter = require('./routes/dishRouter')
 var promoRouter = require('./routes/promoRouter')
 var leaderRouter = require('./routes/leaderRouter')
 const Dishes = require('./models/dishes')
+var config = require('./config');
 
-const url = 'mongodb://localhost:27017/conFusion'
+const url = config.mongoUrl;
 const connect = mongoose.connect(url)
 
 connect.then(db => {
@@ -50,21 +51,7 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.user);
 
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-  }
-  else {
-    next();
-  }
-}
-
-app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
