1819e82c6cb6faa3762844dee1eb84ec44baf081
zhaoyang <zla143@sfu.ca>
2018-09-04 23:04:11 -0700

    oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '335789313655017',
+    clientSecret: '4e92fb4d9a39d6f40f064b6b093ac7a4'
+  }
 }
